6cf6b8caf1fb20b150022f480772ae5be5ef795a
hakanttek <hakanttek@gmail.com>
2025-05-04 23:53:58 +0200

    feat: implement CRUD routes for configuration entities and integrate oconf.router

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require('dotenv');
+var crudRoutes = require('./open-config/oconf.router.js');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,5 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/oconf', oconfRouter);
+app.use(crudRoutes);
 
 module.exports = app;
--- src/open-config/oconf.router.js
+++ src/open-config/oconf.router.js
@@ -3,8 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+
 router.get('/', function (req, res, next) {
   res.send(oconf);
 });
+console.log((oconf?.routes?.crud ?? "") + "/");
+for (const entityName in oconf.entities) {
+  router.get((oconf?.routes?.crud ?? "") + "/" + entityName, function (req, res, next) {
+    res.send(oconf.entities[entityName]);
+  });
+}
 
 module.exports = router;
